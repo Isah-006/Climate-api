f7e3db77e88f6653380126d3d4a0cb7650efa87e
Gabriel Affonso <gabriel.affonsolb@gmail.com>
2026-05-28 18:01:40 -0300

    feat: adiciona criação de conta no frontend

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-// Esta interface serve para avisar o TypeScript que vamos guardar o ID do usuário dentro da requisição
 declare global {
   namespace Express {
     interface Request {
@@ -10,34 +9,48 @@ declare global {
   }
 }
 
-export function autorizarUsuario(req: Request, res: Response, next: NextFunction) {
-  // 1. Procura o Token no cabeçalho (Header) da requisição
+export function autorizarUsuario(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({ erro: 'Token não fornecido.' });
+    return res.status(401).json({
+      erro: 'Token não fornecido.',
+    });
   }
 
-  // O token geralmente vem no formato "Bearer TOKEN_AQUI", vamos dividir e pegar só o token
   const parts = authHeader.split(' ');
+
   if (parts.length !== 2) {
-    return res.status(401).json({ erro: 'Erro no formato do Token.' });
+    return res.status(401).json({
+      erro: 'Erro no formato do token.',
+    });
   }
 
   const [scheme, token] = parts;
 
   if (!/^Bearer$/i.test(scheme)) {
-    return res.status(401).json({ erro: 'Token malformado.' });
+    return res.status(401).json({
+      erro: 'Token mal formatado.',
+    });
   }
 
-  // 2. Valida se o Token é verdadeiro e não expirou
-  jwt.verify(token, 'senha_secreta_da_faculdade', (err, decoded: any) => {
-    if (err) {
-      return res.status(401).json({ erro: 'Token inválido ou expirado.' });
-    }
+  jwt.verify(
+    token,
+    'senha_secreta_da_faculdade',
+    (err, decoded: any) => {
+      if (err) {
+        return res.status(401).json({
+          erro: 'Token inválido ou expirado.',
+        });
+      }
 
-    // Se o token for válido, guarda o ID do usuário dentro da requisição para as próximas funções usarem
-    req.usuarioId = decoded.id;
-    return next(); // Deixa o usuário passar para a rota do clima
-  });
+      req.usuarioId = decoded.id;
+
+      return next();
+    }
+  );
 }
